33c1778634132536aeeef0ec13adde61d86dde91
Janhvi-42 <janhvikumbhar7@gmail.com>
2024-10-28 19:23:02 +0530

    Update

---
+++ commenting program.c
@@ -0,0 +1,17 @@
+//commenting program//
+
+#include<stdio.h>
+int main()
+{
+ //enter your name//
+   printf("Hey my name is Janhvi");
+   return 0;
+   
+}
+
+
+output 
+
+
+Hey my name is Janhvi
+[Process completed - press Enter]
