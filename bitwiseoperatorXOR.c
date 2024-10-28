01fbc7b6a319dc3b4c40d492463b57d67fdf74a5
Janhvi-42 <janhvikumbhar7@gmail.com>
2024-10-28 15:22:40 +0530

    Update

---
+++ bitwiseoperatorXOR.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+  //value of 60=00111100 13=00001101//
+ int p=60,r=0,q=13;
+       r= p^q;
+printf("the value of r is %d",r );
+} 
+
+
+
+output
+the value of r is 49(00110001)
