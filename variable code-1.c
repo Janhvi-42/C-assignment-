06773e0567af9021e1bb4bc9a6f49cde179e60e8
Janhvi-42 <janhvikumbhar7@gmail.com>
2024-10-28 19:20:47 +0530

    Update

---
+++ variable code-1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+   int g;
+   g=13; //g is storing value which is 13//
+   printf("%d",g);
+   return 0;
+   
+}
+
+
+output
+
+13
+[Process completed - press Enter]
