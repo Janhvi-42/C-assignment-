d6f7120e64ddf68859e402e9eec910c63abc1c46
Janhvi-42 <janhvikumbhar7@gmail.com>
2024-10-28 15:26:56 +0530

    Update

---
+++ bitwiseoperatorAND.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+   int p=60;//60=0011 1100//
+int q=13;// 13=0000 1101//
+int r=0
+r= p&q;//49= 0011 0001//
+printf("the value of r is %d",r );
+return 0;
+}
+
+
+
+output:
+the value of r is 12(00001100)
