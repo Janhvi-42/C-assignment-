2e26fde61b6081a8abd5dc5990a96de1d2ac1b49
Janhvi-42 <janhvikumbhar7@gmail.com>
2024-10-28 19:30:14 +0530

    Update

---
+++ program for define declare n usage of variable.c
@@ -0,0 +1,19 @@
+//program for definition declaration n usage//
+
+#include<stdio.h>
+int main()
+{
+ //define a variable//
+     int a,b;
+     a=13;//declare that variable//
+     b=a+1;//usage of that variable//
+   printf("%d\n",b);
+   return 0;
+   
+}
+
+output
+
+14
+
+[Process completed - press Enter]
