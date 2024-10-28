48d5397f94c7ae2ac4682eba97bd091eb0376c3d
Janhvi-42 <janhvikumbhar7@gmail.com>
2024-10-28 19:25:25 +0530

    Update

---
+++ variablechar.c
@@ -0,0 +1,17 @@
+//character program//
+
+#include<stdio.h>
+int main()
+{
+ //enter your name//
+     char ch;
+   printf("enter your character");
+   return 0;
+   
+}
+
+
+output
+
+enter your character
+[Process completed - press Enter]
